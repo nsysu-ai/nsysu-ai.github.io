1e2e975e1078206070c9175c34b4ce6e276a501c
kyumdbot <kyumd001@gmail.com>
2018-12-20 13:19:51 +0800

    update

--- app/face/app.js
+++ app/face/app.js
@@ -162,8 +162,25 @@ videoSelect.onchange = start;
 var mainDiv = document.getElementById("mainDiv");
 mainDiv.style.left = (1200/2 - 500/2) + "px";
 
+var userUUID = "tmpuser@" + generateUUID();
+console.log(userUUID);
+
 start();
 
+
+//---------------------------------------------
+
+function generateUUID() {
+    var d = Date.now();
+    if (typeof performance !== 'undefined' && typeof performance.now === 'function'){
+        d += performance.now(); //use high-precision timer if available
+    }
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+        var r = (d + Math.random() * 16) % 16 | 0;
+        d = Math.floor(d / 16);
+        return (c === 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+    });
+}
 
 //---------------------------------------------
 
